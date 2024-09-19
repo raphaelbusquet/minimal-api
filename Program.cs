d9b6eed7a840fda72dc49eeb4ccf4acde10ba5dd
Raphael Busquet <73319921+raphaelbusquet@users.noreply.github.com>
2024-09-19 23:20:38 +0100

    .

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Interfaces;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+builder.Services.AddScoped<IAdministradorServico, IAdministradorServico>();
 
 builder.Services.AddDbContext<DbContexto>(options => options.UseMySql(
     builder.Configuration.GetConnectionString("mysql"),
@@ -12,8 +16,8 @@ builder.Services.AddDbContext<DbContexto>(options => options.UseMySql(
 
 app.MapGet("/", () => "Hello!");
 
-app.MapPost("/login", (LoginDTO LoginDTO) => {
-    if (LoginDTO.Email == "adm@test.com" && LoginDTO.Senha == "123456") {
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) => {
+    if (administradorServico.Login(loginDTO) != null) {
         return Results.Ok("Login efetuado com sucesso!");
     }
     else 
